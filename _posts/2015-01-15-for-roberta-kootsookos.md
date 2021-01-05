f78c01c07c4abfe7246461a911818a245061c00c
kootsoop <peter@kootsoop.com>
2021-01-05 09:01:18 -0500

    Added first piece of writing for 2020.

---
+++ _posts/2015-01-15-for-roberta-kootsookos.md
@@ -0,0 +1,56 @@
+# For Roberta Kootsookos
+
+We are here to celebrate the life of my Mum, Roberta Kootsookos, Bobbie.
+
+Alex has chosen not to speak today, but has asked me to read this poem:
+
+ > Magnificent food and a happy cook
+ > A designer with an original eye
+ > Funny and clever
+ > Interested in the world
+ > Always willing to listen to the tone of voice as well as the words
+ > Knowing all my secrets
+ > Fair and honest and kind
+ > The person who listens when I am annoyed with friends
+ > The one I talk to about life decisions
+ > The person I can just sit with
+ > That special someone I've known all my life and who I will adore to the end of my days.
+ > Mother.
+
+Mum was born in Townsville during World War 2. Because Pa, my grandfather Bob Simpson, went away to the war, Nana (Et Simpson nee Higgs) and Mum moved down to Brisbane to be with relatives. She stayed quite close to here during that time.
+
+Mum grew up in Lawson Street in Townsville. Dad also grew up in Townsville. Alex and I were both born there.
+
+What I know of Mum is that she enjoyed life. She loved cooking. She loved parties. She loved travel. She loved houses. She loved her friends. She loved her family.
+
+Mum's cooking started off relatively traditional, but as she learned more from her travels, she used more Asian and spicier influences. When I moved out of home, one of the things she gave me was an exercise book with some of our family favourite recipes in it. Simple things, but yummy.
+
+One of my earliest memories of parties at our place in Townsville was being allowed to take the finger food tray around to all the guests. Mum continued to like entertaining throughout her life. We had a small gathering at Wheller on the Park, where Mum lived, on the day before she passed. I see several people here now who were there then. She had such a great time that day, talking to everyone and being able to show off Aoife and Eilis.
+
+Mum built, or re-built, or just renovated many houses in her time. All of them had a sense of style and livability that is hard to find in most places. The picture in the remembrance is of the small house in Milton that she gutted back to the studs and re-built, but there were many others. All of them were her home. Alex and I, and my own family, were always welcome.
+
+Mum and Dad travelled to Europe in the early 70's. As a family, we visited New Zealand in 1972. The biggest trip we had as a family was probably when we visited London, Paris, and Hong Kong in 1977.
+
+I think Mum's best trip, though, was with her friend Kay McCallum (Leahy). Kay was wanting to go on a European trip, but her husband was not interested so she asked Mum if she'd go too. Mum jumped at the chance. As well as Europe, they travelled to Canada (to see Claire Johnson) and the USA.
+
+Mum always said that in nine weeks of close travel with Kay, they never had a cross word.
+
+There are others, but the three people I think Mum was closest to were Maureen, Gail, and Eleanor. Maureen and Gail managed to travel to my wedding with Aine, in Dornoch in Scotland. More travel!
+
+Since moving to Wheller, and becoming sick, Mum received the generous support of many of her friends: Jo Gamble, Joan Goodwin, Jenny Godwin, Ann Jones, Claire Godier.
+
+I have to apologise to Jo, Joan, and Jenny because all the JGs confused me a bit before I had a chance to meet you and put faces to your names.
+
+I know I've missed friends in that list. Forgive me; Mum was happy with all of you. I've only had a short time to understand what Mum was going through these last months.
+
+For Dad, Alex, and me, Mum was the centre of our family. Dad was a workaholic, so was away with business quite a bit. Alex and I were both study-a-holics. Mum made sure we had a home.
+
+Mum's family, her relatives, was always the most important to her. As an only child, her cousins were very important to her growing up. Jill and Jackie have been giving her non-medical outings to look forward to. Thank-you for that.
+
+Mum was close to my Aunt Chris's kids. Especially the flower girl in Mum and Dad's wedding photos, Isabella Shatte. I think Mum saw much of herself in Isabella, and supported her periodically through hard times and good times. It was good to catch up with Isabella and her husband, Brian for dinner last night.
+
+Mum was so happy when Aine and I returned to Brisbane, and announced that Aine was pregnant with Aoife. Mum didn't take over, but was there as our support when we needed it as new parents. Mum was happy again when Eilis was born: as well as another grandchild, she had a chance to travel to the US to meet her.
+
+I know many of you knew different aspects of Bobbie. Please remember the best parts of her. Keep her in mind, and be happy with a life well-lived.
+
+Thank-you all for coming today.
---
+++ _posts/process.sh
@@ -0,0 +1,48 @@
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' "2018-09-24-Cheat--ethically--d5180a96ccd9.md"
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-05-Take-A-Number--Requirements---OOA-bb2eb6f16e3c.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-06-Take-A-Number--High-Level-Architecture-1c7db43a4d37.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-06-Take-A-Number--User-Experience-30195b0e78f8.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-07-Take-A-Number--Technology-Selection-918c7eccc4b4.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-07-Take-A-Number--User-Experience--Take-2-fe7bc70ca0b8.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-11-Take-A-Number---Project-Organization-5120240c4468.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-11-Take-A-Number--Analysis-to-Design-b005ffef47ac.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-12-Take-A-Number---First-Cut-8222ad39b729.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-12-Take-A-Number--First-Performance-3914bb61d5c4.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-15-Block-H-H-H-H-HDistributed-Ledger-Technology-79128dd2e8d7.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-15-Take-A-Number---Things-Undone-b3b298ae1db2.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-03-25-Take-A-Number--First-Use-df6cf0338aa7.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-09-19-Starting-In-Programming.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2019-11-27-Come-On-Down.md
+#sed -i -e 's+https://medium.com/@kootsoop/+https://kootsoop.github.io/@kootsoop-+g' 2020-03-30-covid19.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2018-09-24-Cheat--ethically--d5180a96ccd9.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-05-Take-A-Number--Requirements---OOA-bb2eb6f16e3c.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-06-Take-A-Number--High-Level-Architecture-1c7db43a4d37.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-06-Take-A-Number--User-Experience-30195b0e78f8.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-07-Take-A-Number--Technology-Selection-918c7eccc4b4.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-07-Take-A-Number--User-Experience--Take-2-fe7bc70ca0b8.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-11-Take-A-Number---Project-Organization-5120240c4468.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-11-Take-A-Number--Analysis-to-Design-b005ffef47ac.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-12-Take-A-Number---First-Cut-8222ad39b729.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-12-Take-A-Number--First-Performance-3914bb61d5c4.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-15-Block-H-H-H-H-HDistributed-Ledger-Technology-79128dd2e8d7.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-15-Take-A-Number---Things-Undone-b3b298ae1db2.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-03-25-Take-A-Number--First-Use-df6cf0338aa7.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-09-19-Starting-In-Programming.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2019-11-27-Come-On-Down.md
+#sed -i '' -e 's+https://cdn-images-1.medium.com/max/800/1*+https://kootsoop.github.io/images/1_+g' 2020-03-30-covid19.md
+sed -i '' -e 's+1_\*+1_+g' 2018-09-24-Cheat--ethically--d5180a96ccd9.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-05-Take-A-Number--Requirements---OOA-bb2eb6f16e3c.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-06-Take-A-Number--High-Level-Architecture-1c7db43a4d37.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-06-Take-A-Number--User-Experience-30195b0e78f8.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-07-Take-A-Number--Technology-Selection-918c7eccc4b4.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-07-Take-A-Number--User-Experience--Take-2-fe7bc70ca0b8.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-11-Take-A-Number---Project-Organization-5120240c4468.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-11-Take-A-Number--Analysis-to-Design-b005ffef47ac.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-12-Take-A-Number---First-Cut-8222ad39b729.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-12-Take-A-Number--First-Performance-3914bb61d5c4.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-15-Block-H-H-H-H-HDistributed-Ledger-Technology-79128dd2e8d7.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-15-Take-A-Number---Things-Undone-b3b298ae1db2.md
+sed -i '' -e 's+1_\*+1_+g' 2019-03-25-Take-A-Number--First-Use-df6cf0338aa7.md
+sed -i '' -e 's+1_\*+1_+g' 2019-09-19-Starting-In-Programming.md
+sed -i '' -e 's+1_\*+1_+g' 2019-11-27-Come-On-Down.md
+sed -i '' -e 's+1_\*+1_+g' 2020-03-30-covid19.md
